d0b361560b6fa217e6010cff5e8ed78580106bb8
yeison <yeisonferneyhoyos@gmail.com>
2023-10-18 17:32:25 -0500

    inpeccionando el dashboard.jsx

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from 'react'
 import { useAuthContext } from '../hooks/useAuthContext' 
-import { getMeUserService } from '../services/userServices'
+import { getMeUserService } from './services/userServices'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../footer/Footer'
 import './Dashboard.css'
