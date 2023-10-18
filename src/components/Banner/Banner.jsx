5e0b73061091a1d96b2fbec6d40186ccb629fa31
yeison <yeisonferneyhoyos@gmail.com>
2023-10-18 17:12:00 -0500

    cambios

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -25,7 +25,7 @@ const Banner = () => {
     <img id="img4" className="banner-img" src="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2022/02/25/16458002845779.gif" alt="" />
     <img id="img5" className="banner-img" src="https://vangogh.teespring.com/v3/image/12EVK10jQxRf-PlivW0xr0YToz4/480/560.jpg" alt="" />
     <img id="img6" className="banner-img" src="https://target.scene7.com/is/image/Target/GUEST_f546c66a-45d9-4b2d-92a0-e9420a0ea6ac?wid=488&hei=488&fmt=pjpeg" alt="" />
-    <img id="img7" className="banner-img" src="https://www.ld-aromaticos.com/imagenes/productos/Fresh%20New%20Car-Spray%2060%20ml-Coche%20nuevo." alt="" />
+    <img id="img7" className="banner-img" src="" alt="" />
     <img id="img8" className="banner-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzPfe_T8fvc1uKYYQtzOnlXP2YdX96iRx18Q&usqp=CAU" alt="" />
     <img id="img9" className="banner-img" src="https://http2.mlstatic.com/D_NQ_753106-MLA71830383654_092023-OO.webp" alt="" />
     <img id="img10" className="banner-img" src="https://http2.mlstatic.com/D_NQ_807444-MLA54784979760_032023-OO.webp" alt="" />
